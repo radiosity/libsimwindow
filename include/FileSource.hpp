72394282b9fb81e52180667ed5f56e122cf30c10
Richard Martin <radiosity@gmail.com>
2013-04-28 02:51:28 +0100

    OK, another step along the road. The FileSource now pulls in the DataSource class and the heirarchy is sorted out. It's fairly blank for now.
    Also, rejigged some of the move constructors, some more comments.

--- include/FileSource.hpp
+++ include/FileSource.hpp
@@ -29,11 +29,13 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #ifndef FileSource_HEADER
 #define FileSource_HEADER
 
+#include <vector>
 #include <string>
 #include <utility>
 #include <fstream>
 #include <future>
 #include <functional>
+#include <cmath>
 
 #include "DataSource.hpp"
 
@@ -43,41 +45,117 @@ using std::ifstream;
 using std::future;
 using std::async; 
 using std::function; 
+using std::packaged_task;
+using std::vector;
 
 namespace libsim 
 {
-
 	
 template <class T>
 class FileSourceImpl;
 	
 template <class T>
-class FileSource : public DataSource {
+class FileSource : public DataSource<T> {
+	
+	private:
+		auto_ptr<FileSourceImpl<T>> impl;
 	
 	public:
-		FileSource(string filename, unsigned int windowsize, int datapoints = -1 )
-	{
-	
-	}
-
+		FileSource(string _filename, unsigned int _windowsize, int _datapoints = -1 ) : DataSource<T>(_windowsize)
+		{
+			impl = auto_ptr<FileSourceImpl<T>>(new FileSourceImpl<T>(_filename, _windowsize, _datapoints));
+		}
+		
+		//No copying. That would leave this object in a horrendous state
+		//and I don't want to figure out how to do it. 
+		FileSource(FileSource<T> const & cpy) = delete; 
+		FileSource<T>& operator =(const FileSource<T>& cpy) = delete; 
+		
+		//Moving is fine, so support rvalue move and move assignment operators.
+		FileSource(FileSource<T> && mv) : DataSource<T>(mv.windowsize), impl(mv.impl) {}
+		FileSource<T>& operator =(FileSource<T> && mv) { impl = mv.impl; return *this; }
+		~FileSource() = default; 
+		
+		T get() const override { return 0; };
+		void tock() override {};
 
 };
 
 template <class T>
-class FileSourceImpl : public DataSource {
+class FileSourceImpl {
 	
 	private:
-		auto_ptr<T> data;
+		vector<T> data;
 		ifstream file;
-		future<void> func; 
+		future<void> ft; 
+	
+		int datapoints_limit; 
+		unsigned int datapoints_read; 
+		unsigned int windowsize; 
+		unsigned int start; 
+	
+		bool ready; 
+	
+		unsigned int read_extent;
 		
 	public:
-		FileSourceImpl(string filename, unsigned int windowsize, int datapoints = -1 ) :
-			data(NULL), 
-			file(filename) 
+		FileSourceImpl(string filename, unsigned int _windowsize, int datapoints = -1)  :
+			data(), 
+			file(filename), 
+			ft(),
+			datapoints_limit(datapoints),
+			datapoints_read(0),
+			windowsize(_windowsize),
+			start(0),
+			ready(false)
 		{
-	
+			
+			//First, calculate the read extent from the window size. 
+			//What I'd like to do is to read at least three times the 
+			//windowsize, rounded up to the nearest multiple of 1024.
+			//
+			//1024 is by no means a magic number supposed to align the 
+			//read to a page boundary or somesuch; the input file is 
+			//ASCII formatted T, so the number of lines read will only 
+			//losely conform to a number of bytes. 1024 is Just a nice number. 
+			
+			read_extent = 
+				(unsigned int) 
+					floor(
+						(
+							(
+								(double) (windowsize * 3)
+							)
+						) / 1024.0
+					) * 1024;
+			
+			data.reserve(read_extent);
+
+			auto func = [&]() {
+				
+				for(int i = 0; i < read_extent; i++)  {
+					if(datapoints_read == datapoints_limit) break; 
+					file >> data[i];
+					datapoints_read++;
+				}
+				
+				ready = true; 
+				
+			};
+			
+			ft = async(func);
+			
+			
 		}
+		
+		//Absolutely no copying. 
+		FileSourceImpl(FileSourceImpl<T> const & cpy) = delete; 
+		FileSourceImpl<T>& operator =(const FileSourceImpl<T>& cpy) = delete; 
+		
+		
+		FileSourceImpl(FileSourceImpl<T> && mv) = delete; 
+		FileSourceImpl<T>& operator =(FileSourceImpl<T> && mv) = delete; 
+		~FileSourceImpl() = default; 
 
 };
 
